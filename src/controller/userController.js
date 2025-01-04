6bed17fdfb0cb369625d405ff4da91b723075028
Bikram Sahoo <bikramsahoo@live.in>
2025-01-04 19:04:16 +0530

    Impliment Forgot password feature.

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -35,6 +35,7 @@ async function loginUser(req,res){
 
             bcrypt.compare(data.password, userData.password, function(err, match) {
                 if(!err){
+                    console.log(match);
                     if(match){
                         delete userData.password
                         const token = jwt.sign(userData,'jwt-secKey');
@@ -53,4 +54,53 @@ async function loginUser(req,res){
     }
 }
 
-module.exports = {registerUser,loginUser}
+async function forgotPassword(req,res){
+    const data = req.body;
+    try {
+        const q = 'SELECT * FROM "UserData" WHERE email = $1';
+        const value = [data.email];
+        const result = await client.query(q,value);
+        const userData = result.rows[0];
+        if(userData){
+            delete userData.password;
+            res.status(200).json({status:'success',message:'OTP Sent Successfully.'});
+        }else{
+            res.status(400).json({status:'failure',message:'User Not Found.'});
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:'Internal Server Error.'});
+        
+    }
+}
+function verifyPassword(req,res){
+    const {otp,email,password} = req.body;
+    
+    if(otp=='123456'){
+        
+        const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, async function(err, hash) {
+        try {
+            const q = `UPDATE "UserData" SET password = $1 WHERE email = $2 RETURNING *;`;
+    const value = [hash,email];
+    const result = await client.query(q,value);
+    console.log(result.rows[0]);
+    if(result.rows[0]){
+        res.status(200).json({status:'success',message:"Password Updated Successfully."});
+    }else{
+        res.status(500).json({status:'false',message:'Error while update password'});
+    }
+    
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({status:'false',message:'Internal Server Error!'});
+        }
+    })
+
+    }else{
+        res.status(400).json({status:"failure",message:"Invalid OTP."});
+    }
+}
+
+module.exports = {registerUser,loginUser,forgotPassword,verifyPassword}
--- src/router/userRouts.js
+++ src/router/userRouts.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const UserRouter = express.Router();
 
-const {registerUser,loginUser} = require('../controller/userController');
+const {registerUser,loginUser,forgotPassword,verifyPassword} = require('../controller/userController');
 
 UserRouter.route('/register')
 .post(registerUser);
 
 UserRouter.route('/login')
 .post(loginUser);
+
+UserRouter.route('/forgotPassword')
+.post(forgotPassword);
+
+UserRouter.route('/verifyPassword')
+.post(verifyPassword);
 
 
 module.exports = UserRouter;
